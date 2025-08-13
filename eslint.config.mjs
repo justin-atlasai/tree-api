20b747396b3dc6a3a367aaf14d922f4ec09c0188
Justin Runes <justin@seveti.com>
2025-08-13 15:47:24 -0500

    ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,15 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [".next/**", "dist/**", "build/**"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unsafe-function-type": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-    },
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {},
   },
 ];
 
